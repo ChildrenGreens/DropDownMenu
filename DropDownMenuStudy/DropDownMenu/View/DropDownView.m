bdfa78a980fbd20c2e3bc0e7236668e53e5ba259
caiqiujun <903400858@qq.com>
2016-02-04 14:15:36 +0800

    制作主滑动视图完成！

--- DropDownMenuStudy/DropDownMenu/View/DropDownView.m
+++ DropDownMenuStudy/DropDownMenu/View/DropDownView.m
@@ -8,12 +8,15 @@
 
 #import "DropDownView.h"
 #import "BottomView.h"
+#import "MainTableViewCell.h"
 
-@interface DropDownView ()
-/// 灰色背景视图
-@property (nonatomic, strong)UIView *backView;
+@interface DropDownView ()<UITableViewDataSource, UITableViewDelegate>
 /// 灰色背景视图
 @property (nonatomic, strong)UIView *downView;
+/// 主选项卡
+@property (nonatomic, strong)UITableView *mainTableView;
+/// 辅选项卡
+@property (nonatomic, strong)UITableView *secondaryTableView;
 
 @end
 
@@ -27,9 +30,12 @@
         // 增加单机手势
         UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideDropDownView:)];
         self.userInteractionEnabled = YES;
+        gesture.cancelsTouchesInView = NO;
         [self addGestureRecognizer:gesture];
-        
+        // 增加下拉视图
         [self showDropDownView];
+        // 设置主选项卡
+        [self addMainTab];
         
         [self setBottomView];
     }
@@ -37,10 +43,32 @@
 }
 
 /**
+ *  主选项卡
+ */
+-(void)addMainTab {
+    self.mainTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width * 0.32, self.downView.frame.size.height - self.downView.frame.size.width * 0.048)];
+    [self.mainTableView registerClass:[MainTableViewCell class] forCellReuseIdentifier:@"mainCell"];
+    self.mainTableView.rowHeight = 38;
+    self.mainTableView.dataSource = self;
+    self.mainTableView.delegate = self;
+    self.mainTableView.separatorColor = [UIColor colorWithWhite:0.8 alpha:0.5];
+    [self.downView addSubview:self.mainTableView];
+    UIView *header = [[UIView alloc] initWithFrame:CGRectZero];
+    self.mainTableView.tableFooterView = header;
+    
+}
+
+/**
+ *
+ */
+
+
+
+/**
  *  设置底部边框
  */
 - (void)setBottomView {
-    BottomView *bottomView = [[BottomView alloc] initWithFrame:CGRectMake(0, self.downView.frame.size.height - self.downView.frame.size.width * 0.06, self.downView.frame.size.width, self.downView.frame.size.width * 0.06)];
+    BottomView *bottomView = [[BottomView alloc] initWithFrame:CGRectMake(0, self.downView.frame.size.height - self.downView.frame.size.width * 0.048, self.downView.frame.size.width, self.downView.frame.size.width * 0.048)];
     [self.downView addSubview:bottomView];
 }
 
@@ -90,6 +118,25 @@
         
         [self hideDropDownView];
     }
+}
+
+#pragma mark UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MainTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"mainCell" forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
 }
 
 @end
---
+++ DropDownMenuStudy/DropDownMenu/View/TableViewCell/MainTableViewCell.m
@@ -0,0 +1,31 @@
+
+//
+//  MainTableViewCell.m
+//  DropDownMenuStudy
+//
+//  Created by caiqiujun on 16/2/3.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+#import "MainTableViewCell.h"
+
+@interface MainTableViewCell ()
+
+@end
+
+@implementation MainTableViewCell
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+
+
+@end
